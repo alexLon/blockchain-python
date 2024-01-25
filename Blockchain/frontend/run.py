517fe88f951c46af63e4b088c616d75088a6f6b2
alexLon <151473647+alexLon@users.noreply.github.com>
2024-01-25 18:52:19 +0000

    feature - first draft of the website, will add more pages and probably refine the design later

--- Blockchain/frontend/run.py
+++ Blockchain/frontend/run.py
@@ -1,10 +1,52 @@
 from flask import Flask, render_template, request
 from Blockchain.client.sendBTC import SendBTC
 from Blockchain.backend.core.transaction import Tx
+from Blockchain.backend.core.database.database import BlockChainDB
+import time 
 
+timestamp = time.time()
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route('/block')
+def block():
+    return "<h1>Block</h1>"
+
+@app.route('/transactions')
+def transactions():
+    return "<h1>Transactions</h1>"
+
+@app.route('/mempool')
+def mempool():
+    return "<h1>Mempool</h1>"
+
+@app.route("/search")
+def search():
+    return "<h1>search</h1>"
+
+""" Read data from the blockchain """
+def read_database():
+    errorFlag = False
+    while errorFlag:
+        try:
+            blockchain = BlockChainDB()
+            blocks = blockchain.read()
+            errorFlag = False
+        except:
+            errorFlag = True
+            print("Error reading database")
+    return blocks
+
+@app.route("/block")
+def block():
+    blocks = read_database()
+    return render_template('block.html', blocks=blocks)
+
+
+@app.route('/wallet', methods = ['GET', 'POST'])
 def wallet():
     message = ''
     if request.method == 'POST':
